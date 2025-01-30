fe4fee557c7a58ce3d52e42db2dea9eaeb8c8673
ZETRO <110453029+ZETRO2@users.noreply.github.com>
2025-01-30 09:02:09 -0800

    Update Esp32_Internet_Radio_Code.ino

--- Esp32_Internet_Radio_Code.ino
+++ Esp32_Internet_Radio_Code.ino
@@ -189,13 +189,6 @@ void nextStation() {
     connectToHost();  // Connect to the new station
 }
 
-
-void nextStation() {
-    currentStation = (currentStation + 1) % totalStations;
-    displayStation();
-    connectToHost();
-}
-
 void previousStation() {
     currentStation = (currentStation - 1 + totalStations) % totalStations;
     displayStation();
